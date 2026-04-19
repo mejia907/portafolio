5fd25d90dbf7927ee84927100524a35fc92303b9
Andres Felipe Mejia <mejia9070@gmail.com>
2026-04-19 13:21:09 -0500

    fix: actualziacion de librerias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [icon()],
 });
